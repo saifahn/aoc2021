baac78d0897eb81c449828bd59f0b8829f25dabe
Sean Li <li.saifahn@gmail.com>
2021-12-07 23:18:53 +0900

    day 7 part 1

--- cmd/07-treachery-of-whales/treachery-of-whales.go
+++ cmd/07-treachery-of-whales/treachery-of-whales.go
@@ -4,21 +4,67 @@ import (
 	"aoc/utils"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 )
 
-func parseCrabs(lines []string) []string {
+func parseCrabs(lines []string) []int {
 	if len(lines) != 1 {
 		log.Fatal("expected just one line")
 	}
-	return strings.Split(lines[0], ",")
+	crabs := strings.Split(lines[0], ",")
+	var crabInts []int
+	for _, c := range crabs {
+		crab, _ := strconv.Atoi(c)
+		crabInts = append(crabInts, crab)
+	}
+	return crabInts
+}
+
+func getRange(crabs []int) (min, max int) {
+	// iterate through crabs
+	min, max = crabs[0], crabs[0]
+	for _, crab := range crabs {
+		if crab < min {
+			min = crab
+		}
+		if crab > max {
+			max = crab
+		}
+	}
+	return min, max
+}
+
+func calculateLeastFuelNeeded(crabs []int, min, max int) int {
+	leastFuel := 0
+	for x := min; x <= max; x++ {
+		total := 0
+		for _, crab := range crabs {
+			distance := crab - x
+			// get the absolute value
+			if distance < 0 {
+				distance = -distance
+			}
+			total += distance
+		}
+		if total < leastFuel {
+			leastFuel = total
+		}
+		if leastFuel == 0 {
+			leastFuel = total
+		}
+	}
+	return leastFuel
 }
 
 func main() {
-	lines, err := utils.ReadLines("data/07-test")
+	lines, err := utils.ReadLines("data/07-real")
 	if err != nil {
 		log.Fatalf("could not read lines: %s", err)
 	}
 	crabs := parseCrabs(lines)
-	fmt.Printf("%v", crabs)
+	min, max := getRange(crabs)
+	leastFuel := calculateLeastFuelNeeded(crabs, min, max)
+	// fmt.Printf("%v, %v", crabs)
+	fmt.Printf("%v\n", leastFuel)
 }
